248f6ef6871ac9989b3f4ef705b3d44548c2c02a
Aschweik766 <ashnjackson766@gmail.com>
2022-06-30 18:47:03 -0600

    added index.js and connection boilerplates.

---
+++ Database/connection.js
@@ -0,0 +1,20 @@
+
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+
+mongoose.Promise = Promise
+
+const mongoURI =
+    process.env.NODE_ENV === 'production' 
+    ? process.env.DB_URL
+    : process.env.DB_URL
+    
+
+mongoose.connect(mongoURI)
+    .then((instance)=>
+    console.log(`Connected to db: ${instance.connections[0].name}`)
+    )
+    .catch((error) => console.log(`Connection failed`, error));
+
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //planning: boilerplate to begin here, setup localhost port
+const express = require('express')
+const app = express()
+
+const dot = require('dotenv').config()
+const cors = require('cors')
+
+const methodOverride = require('method-override')
+const userController = require('./Controller/user-router')
+const exerciseController = require('./Controller/exercise-router')
+
+
+app.use(cors()) // to prevent cors errors, open access to all origins
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use('/public', express.static(__dirname + 'public'))
+app.use(methodOverride('_method'))
+
+app.use(userController)
+
+
+
+const port = process.env.PORT || 3001
+app.listen(port, ()=> {
+    console.log(`server running on port: ${port}`)
+});
