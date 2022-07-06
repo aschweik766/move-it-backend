d05f48e2148c396212adfda68eb9fe56f5a05663
Aschweik766 <ashnjackson766@gmail.com>
2022-07-06 16:55:09 -0500

    add to heroku

--- Controller/exercise-router.js
+++ Controller/exercise-router.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const res = require('express/lib/response')
-const Users = require('../models/exercise/top-exercise-model')
+const Exercise = require('../models/exercise/top-exercise-model')
 const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Hello Exercise App')
+})
+
+router.get('/exercises', (req, res) => {
+    const results = Exercise.find({})
+    results.then((data) => {
+        res.send(data)
+    })
+})
 
 module.exports = router
