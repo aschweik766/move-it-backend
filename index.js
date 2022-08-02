55d5db0e2159060a5bf2a056428dd4a52a3e0d4a
Aschweik766 <ashnjackson766@gmail.com>
2022-08-02 16:21:16 -0600

    enable all cors for simple use.

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.use(express.urlencoded({extended: true}))
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "https://moveit-frontend.herokuapp.com/",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true,
+    // origin: "https://moveit-frontend.herokuapp.com/",
+    // methods: "GET,POST,PUT,DELETE",
+    // credentials: true,
   })
 );
 
