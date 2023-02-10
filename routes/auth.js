8341bd256e8a56d19655fdf33a8e8a49c9bb140e
Aschweik766 <ashnjackson766@gmail.com>
2023-02-09 18:45:32 -0700

    update / route.

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const { check_user } = require("../middlewares/authMiddleware.js");
 
 const router = require("express").Router();
 
-router.post("/verify", check_user); 
+router.post("/", check_user); 
 router.post("/register", register);
 router.post("/login", login);
 
